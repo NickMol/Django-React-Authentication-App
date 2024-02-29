6e9e6f3a647ef11621acadec26fddd3101236c8e
Nick Mol <nickmol96@hotmail.com>
2024-02-29 18:26:11 +0100

    tutorial 8

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,10 +4,33 @@ import MyTextField from './forms/MyTextField'
 import MyPassField from './forms/MyPassField'
 import MyButton from './forms/MyButton'
 import {Link} from 'react-router-dom'
+import {useForm} from 'react-hook-form'
+import AxiosInstance from './AxiosInstance'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () =>{
+    const navigate = useNavigate()
+    const {handleSubmit, control} = useForm()
+
+    const submission = (data) => {
+        AxiosInstance.post(`login/`,{
+            email: data.email, 
+            password: data.password,
+        })
+
+        .then((response) => {
+            console.log(response)
+            localStorage.setItem('Token', response.data.token)
+            navigate(`/home`)
+        })
+        .catch((error) => {
+            console.error('Error during login', error)
+        })
+    }
+
     return(
         <div className={"myBackground"}> 
+            <form onSubmit={handleSubmit(submission)}>
             <Box className={"whiteBox"}>
 
                 <Box className={"itemBox"}>
@@ -17,18 +40,23 @@ const Login = () =>{
                 <Box className={"itemBox"}>
                     <MyTextField
                     label={"Email"}
+                    name ={"email"}
+                    control={control}
                     />
                 </Box>
 
                 <Box className={"itemBox"}>
                     <MyPassField
                     label={"Password"}
+                    name ={"password"}
+                    control={control}
                     />
                 </Box>
 
                 <Box className={"itemBox"}>
                     <MyButton 
                         label={"Login"}
+                        type={"submit"}
                     />
                 </Box>
 
@@ -38,6 +66,8 @@ const Login = () =>{
 
 
             </Box>
+
+        </form>
             
         </div>
     )
