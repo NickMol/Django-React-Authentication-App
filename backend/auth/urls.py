9dc3de4612939b5f996b891dd4bb7d82ae31054b
Nick Mol <nickmol96@hotmail.com>
2024-04-03 18:46:33 +0200

    tutorial 13

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from knox import views as knox_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('users.urls')), 
-    path('api/auth/',include('knox.urls')), 
+    #path('api/auth/',include('knox.urls')), 
+
+    path('logout/',knox_views.LogoutView.as_view(), name='knox_logout'), 
+    path('logoutall/',knox_views.LogoutAllView.as_view(), name='knox_logoutall'), 
     path('api/password_reset/',include('django_rest_passwordreset.urls', namespace='password_reset')), 
 
 ]
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,6 +15,9 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
 import {Link, useLocation} from 'react-router-dom'
+import LogoutIcon from '@mui/icons-material/Logout';
+import AxiosInstance from './AxiosInstance';
+import { useNavigate } from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -22,6 +25,18 @@ export default function Navbar(props) {
   const {content} = props
   const location = useLocation()
   const path = location.pathname
+  const navigate = useNavigate()
+
+  const logoutUser = () =>{
+     AxiosInstance.post(`logoutall/`,{
+     })
+     .then( () => {
+        localStorage.removeItem("Token")
+        navigate('/')
+     }
+
+     )
+  }
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -54,12 +69,21 @@ export default function Navbar(props) {
                 </ListItemButton>
               </ListItem>
 
-              <ListItem key={1} disablePadding>
+              <ListItem key={2} disablePadding>
               <ListItemButton component={Link} to="/about" selected={"/about" === path}>
                   <ListItemIcon>
                         <InfoIcon /> 
                   </ListItemIcon>
                   <ListItemText primary={"About"} />
+                </ListItemButton>
+              </ListItem>
+
+              <ListItem key={3} disablePadding>
+              <ListItemButton onClick={logoutUser}>
+                  <ListItemIcon>
+                        <LogoutIcon/> 
+                  </ListItemIcon>
+                  <ListItemText primary={"Logout"} />
                 </ListItemButton>
               </ListItem>
   
